39b8853142af5347ea8f8e3d9e5cc76801d607ba
Christian <csoronellas@emagister.com>
2012-10-26 15:01:32 +0200

    Updated bootstrap.php

--- test/bootstrap.php
+++ test/bootstrap.php
@@ -4,4 +4,4 @@ if (!file_exists(__DIR__ . '/../vendor/autoload.php')) {
     die('Execute composer before phpuni!!' . PHP_EOL);
 }
 
-require_once __DIR__ . '/../vendors/autoload.php';
+require_once __DIR__ . '/../vendor/autoload.php';
